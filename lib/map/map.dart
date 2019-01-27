2fe0a74aeff9f949327ade4a9523b68e0e276512
Siamak Samie <siamak.samie@gmail.com>
2019-01-26 20:12:29 -0500

    map stuff

--- lib/map/map.dart
+++ lib/map/map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class Map extends StatefulWidget {
   @override
@@ -9,7 +10,10 @@ class Map extends StatefulWidget {
 
 class MapState extends State<Map> {
   GoogleMapController mapController;
-  final LatLng _center = const LatLng(45.495293, -73.579064);
+
+  static Location position = new Location();
+
+  final LatLng _conu = const LatLng(45.495293, -73.579064);
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +23,11 @@ class MapState extends State<Map> {
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
-          options: GoogleMapOptions(
-            cameraPosition: CameraPosition(
-              target: _center,
-              zoom: 11.0,
-            ),
+          myLocationEnabled: true,
+          trackCameraPosition: true,
+          initialCameraPosition: CameraPosition(
+            target: _conu,
+            zoom: 11.0,
           ),
         ));
   }
@@ -32,5 +36,13 @@ class MapState extends State<Map> {
     setState(() {
       mapController = controller;
     });
+
+    mapController.addMarker(
+      MarkerOptions(
+        position: LatLng(37.4219999, -122.0862462),
+        infoWindowText:
+            InfoWindowText("Station Angrignon", "Bikes available 3/10"),
+      ),
+    );
   }
 }
